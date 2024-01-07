9f83b25be80956472c98786d94f0ce53068643d8
tejartr7 <raghusai126@gmail.com>
2024-01-07 22:34:23 +0530

    new commit

---
+++ app/data/data.js
@@ -0,0 +1,63 @@
+const data = [
+    {
+        id: 1,
+        title: 'React js',
+        link:'https://react.dev/',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/React_Logo_SVG.svg/640px-React_Logo_SVG.svg.png',
+        description: 'React is a JavaScript library for building user interfaces. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.',
+    },
+    {
+        id: 2,
+        title: 'Angular js',
+        link:'https://angularjs.org/',
+        image: 'https://miro.medium.com/v2/resize:fit:588/1*15CYVZdpsxir8KLdxEZytg.png',
+        description: 'AngularJS is a JavaScript-based open-source front-end web framework mainly maintained by Google and by a community of individuals and corporations to address many of the challenges encountered in developing single-page applications.',
+    },
+    {
+        id: 3,
+        title: 'Vue js',
+        link:'https://vuejs.org/',
+        image: 'https://avatars.githubusercontent.com/u/6128107?s=280&v=4',
+        description: 'Vue.js is an open-source model–view–viewmodel front end JavaScript framework for building user interfaces and single-page applications. It was created by Evan You, and is maintained by him and the rest of the active core team members.',
+    }, {
+        id: 4,
+        title: 'Next js',
+        link:'https://nextjs.org/',
+        image: 'https://static-00.iconduck.com/assets.00/nextjs-icon-512x512-y563b8iq.png',
+        description: 'Next.js is an open-source development framework built on top of Node.js enabling React based web applications functionalities such as server-side rendering and generating static websites.',
+    },
+    {
+        id: 5,
+        title: 'Nuxt js',
+        link:'https://v2.nuxt.com/',
+        image: 'https://cdn.hashnode.com/res/hashnode/image/upload/v1667629280885/rcnlZ-wkv.png',
+        description: 'Nuxt.js is a free and open source web application framework based on Vue.js, Node.js, Webpack and Babel.js. The framework is advertised as "meta-framework for universal applications".',
+    }, {
+        id: 6,
+        title: 'Gatsby js',
+        link:'https://www.gatsbyjs.com/',
+        image: 'https://www.fermicoding.com/sites/default/files/styles/blogs_mobile_x_1_475px_/public/2021-08/G%20js.png?itok=TE81jrh0',
+        description: 'Gatsby is a React-based open source framework for creating websites and apps. Build anything you can imagine with over 2000 plugins and performance, scalability, and security built-in by default.',
+    },
+    {
+        id: 7,
+        title: 'Swiper js',
+        link:'https://swiperjs.com/',
+        image: 'https://v9.swiperjs.com/images/favicon.png',
+        description: 'Swiper is a free and open source library that allows developers to implement highly customizable and flexible carousel, slider, and swiper functionality into your web app.',
+    }, {
+        id: 8,
+        title: 'Svelte js',
+        link:'https://svelte.dev/',
+        image: 'https://avatars.githubusercontent.com/u/23617963?s=280&v=4',
+        description: 'Svelte is a free and open-source front end JavaScript framework created by Rich Harris and maintained by the Svelte core team members. Svelte applications do not include framework references. Instead, building a Svelte application generates code to manipulate the DOM, which may give better client run-time performance.',
+    },
+    {
+        id: 9,
+        title: 'Node js',
+        link:'https://nodejs.org/en/',
+        image: 'https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_f0b606abb6d19089febc9faeeba5bc05/nodejs-development-services.png',
+        description: 'Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on the V8 engine and executes JavaScript code outside a web browser.',
+    },
+];
+export default data;
---
+++ app/header/Page.jsx
@@ -0,0 +1,12 @@
+
+const Page = () => {
+    return (
+        <div>
+            <nav>
+                <h1 className="text-3xl font-bold"><u>Javascript Frameworks & Libraries!!</u></h1>
+            </nav>
+        </div>
+    )
+}
+
+export default Page
